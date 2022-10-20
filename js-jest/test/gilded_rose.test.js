08bf6f5a9e28e736a538ae769199357083bfc6dc
Matthew Crossley <matthew.crossley@bp.com>
2022-10-20 16:18:21 +0100

    Add aged brie tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,18 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("Aged Brie", function() {
+  it("quality increases over time", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 40)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(41);
   });
+
+  it("quality cannot be over 50", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(50);
+  });
+  
 });
